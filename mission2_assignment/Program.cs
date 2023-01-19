651e09297c3f89e830ad03c881a77fbe191d3518
kizymatzenbacher <kizymatzenbacher@KIZYMATZENBE29C>
2023-01-18 20:29:58 -0700

    Add project files.

---
+++ mission2_assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace mission2_assignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Display Welcome message
+            Console.WriteLine("Welcome to the dice throwing simulator!");
+            //Declare array with size of 13
+            int[] totals = new int[13];
+            //Declare variable to keep track of rolls
+            int rolls;
+
+            //Prompt user to enter number of rolls
+            Console.Write("How many dice rolls would you like to simulate? ");
+
+
+
+        }
+    }
+}
